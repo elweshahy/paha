09f87b5bc98e6a8f7a477d6f6b48dd312adc0e30
sophy <sophyelweshahy@gmail.com>
2024-01-19 21:59:44 +0200

    add on the project

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-alert("hello")
+let sama = document.querySelector("i")
+let mohamed = document.querySelector(".settings-box");
+console.log(sama, mohamed);
+document.querySelector(".toggles .fa-sun").onclick = function(){
+   // document.write("hello world")
+this.classList.toggle("fa-moon");
+
+document.querySelector(".mbroka").classList.toggle("open")
+//document.querySelector(".settings-box").classList.toggle("open")
+}
